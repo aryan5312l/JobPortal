804c82c96758923db7809925e72a4f51fb2b73f3
aryan5312l <aryan10000raj@gmail.com>
2025-02-15 18:23:37 +0530

    debug cookie

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -3,8 +3,8 @@ import jwt from "jsonwebtoken"
 const isAuthenticated = async(req, res, next) => {
     try{
         const token = req.cookies.token;
-        //console.log("Received cookies:", req.cookies);
-        //console.log("Received token:", token);
+        console.log("Received cookies:", req.cookies);
+        console.log("Received token:", token);
         if(!token){
             return res.status(401).json({
                 message: "User not authenticated",
