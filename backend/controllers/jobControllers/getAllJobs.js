4430b41e4cda006c3f0963a51ddaf1ed408727ed
aryan5312l <aryan10000raj@gmail.com>
2025-04-25 16:49:59 +0530

    pagination

--- backend/controllers/jobControllers/getAllJobs.js
+++ backend/controllers/jobControllers/getAllJobs.js
@@ -4,27 +4,37 @@ import { Job } from "../../models/jobModel.js";
 export const getAllJobs = async(req, res) => {
     try {
         const keyword = req.query.keyword || "";
-        
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 9;
+        const skip = (page - 1) * limit;
+
         const query = {
             $or:[
                 {title : {$regex: keyword, $options: "i"}},
                 {description : {$regex: keyword, $options: "i"}},
             ]
         };
-        const jobs = await Job.find(query).populate({
-            path: "company"
-        }).sort({createdAt: -1});
-        if(!jobs){
-            return res.status(404).json({
-                message: "Job Not found",
-                success: false
-            });
-        };
+
+        const totalJobs = await Job.countDocuments(query);
+        const jobs = await Job.find(query)
+        .populate("company")
+        .sort({createdAt: -1})
+        .limit(limit)
+        .skip(skip);
+        
         return res.status(200).json({
             jobs,
+            totalJobs,
+            totalPages: Math.ceil(totalJobs / limit),
+            currentPage: page,
             success: true
         })
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error",
+            error: error.message
+        })
     }
 }
